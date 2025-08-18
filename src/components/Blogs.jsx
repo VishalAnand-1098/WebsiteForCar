6e289cebd4e7fe72bb2faff5e2a2e0926ab6aff5
Kratika Mittal <kratika.mittal@simplifyingskills.com>
2025-08-18 22:50:04 +0530

    "blogs"

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,31 @@
+import About from "../assets/About.webp";
+export default function Blogs() {
+    return (
+        <div>
+            <div
+        style={{
+          width: "100%",
+          height: "350px",
+          marginBottom: "40px",
+          backgroundImage: `url(${About})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <h1
+          style={{
+            color: "white",
+            fontSize: "3rem",
+            fontWeight: "500",
+            marginLeft: "40px",
+            fontFamily: "Jost",
+          }}
+        >
+          Blogs
+        </h1>
+      </div>
+        </div>
+    );
+}
